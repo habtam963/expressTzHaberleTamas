9082addbad9d9d778e4238ecc5b94184ab5ba57b
habtam963 <habtam963@hengersor.hu>
2025-04-10 14:19:12 +0200

    plusz feladatokkal ellatott
    WHERE,  ORDER BY

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection(
     {
 
         user:"root",
-        host:"127.0.0.1",
+        host:"localhost",
         port:3306,
         password:"",
         database:"fogado"
@@ -28,6 +28,31 @@ app.get("/", (req,res) => {
         return res.json(result); 
     });
 });
+
+
+app.get("/potagy", (req,res) => {
+
+    const sql = "SELECT * FROM szobak WHERE potagy='2'";
+
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result); 
+    });
+});
+
+app.get("/vendegek", (req,res) => {
+
+    const sql = "SELECT * FROM vendegek ORDER BY irsz;";
+
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result); 
+    });
+});
+
+
+
+
 
 app.listen(1111, () => {
     console.log("fut a server 1111 porton");
